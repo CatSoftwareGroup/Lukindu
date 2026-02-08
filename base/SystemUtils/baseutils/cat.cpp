258b9d57dc1b7ff7edd3d5fc75963763af09d576
Hawk2811 <2811.pedro@gmail.com>
2026-02-08 02:34:48 -0300

    code of cat command

--- base/SystemUtils/baseutils/cat.cpp
+++ base/SystemUtils/baseutils/cat.cpp
@@ -12,6 +12,17 @@
 
 using namespace std;
 
-int main(){
+string text;
 
+int main(int argc, char const *argv[]){
+
+    if (argc < 2) {
+        cout << argv[0] << ": Usage " << argv[0] << " filename";
+    } else {
+        ifstream input({argv[1]});
+
+        while (getline (input, text)) {
+            cout << text;
+        }
+    }
 }
