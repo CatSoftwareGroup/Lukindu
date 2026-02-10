44b17363f13115f7f7432f59e618a5d0bf477f90
Hawk2811 <2811.pedro@gmail.com>
2026-02-10 03:32:02 -0300

    Add long and hidden listing to ls.cpp

--- base/SystemUtils/baseutils/ls.cpp
+++ base/SystemUtils/baseutils/ls.cpp
@@ -33,7 +33,14 @@ int main(int argc, char const *argv[]){
             fullcommand = listcommand + " " + pathtolist;
             system(fullcommand.c_str());
         } else if (argv[1] == "-la"){
-
+            listcommand = "dir /a";
+            if (argc < 3){
+                pathtolist = ".";
+            } else {
+                pathtolist = argv[2];
+            }
+            fullcommand = listcommand + " " + pathtolist;
+            system(fullcommand.c_str());
         } else if (strcmp(argv[1], "-a") == 0){
             listcommand = "dir /a /b";
             if (argc < 3){
