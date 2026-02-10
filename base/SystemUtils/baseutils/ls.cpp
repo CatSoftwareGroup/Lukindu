a29bf237937ff88e9e4f2675fc12a33d3ab0c192
Hawk2811 <2811.pedro@gmail.com>
2026-02-10 03:40:18 -0300

    Add all temporary functions to ls.cpp

--- base/SystemUtils/baseutils/ls.cpp
+++ base/SystemUtils/baseutils/ls.cpp
@@ -23,42 +23,50 @@ int main(int argc, char const *argv[]){
         string pathtolist;
         string listcommand;
         string fullcommand;
-        if (strcmp(argv[1], "-l") == 0){
-            listcommand = "dir";
-            if (argc < 3){
-                pathtolist = ".";
-            } else {
-                pathtolist = argv[2];
-            }
-            fullcommand = listcommand + " " + pathtolist;
-            system(fullcommand.c_str());
-        } else if (argv[1] == "-la"){
-            listcommand = "dir /a";
-            if (argc < 3){
-                pathtolist = ".";
-            } else {
-                pathtolist = argv[2];
-            }
-            fullcommand = listcommand + " " + pathtolist;
-            system(fullcommand.c_str());
-        } else if (strcmp(argv[1], "-a") == 0){
-            listcommand = "dir /a /b";
-            if (argc < 3){
-                pathtolist = ".";
-            } else {
-                pathtolist = argv[2];
-            }
-            fullcommand = listcommand + " " + pathtolist;
-            system(fullcommand.c_str());
-        } else if (argv[1] == "-h"){
-            helpdialog();
-        } else if (argv[1] == "--help"){
-            helpdialog();
-        } else {
+        if (argc < 2) {
             listcommand = "dir /b ";
             pathtolist = ".";
             fullcommand = listcommand + pathtolist;
             system(fullcommand.c_str());
+        } else {
+            if (strcmp(argv[1], "-l") == 0){
+                listcommand = "dir";
+                if (argc < 3){
+                    pathtolist = ".";
+                } else {
+                    pathtolist = argv[2];
+                }
+                fullcommand = listcommand + " " + pathtolist;
+                system(fullcommand.c_str());
+            } else if (strcmp(argv[1], "-la") == 0){
+                listcommand = "dir /a";
+                if (argc < 3){
+                    pathtolist = ".";
+                } else {
+                    pathtolist = argv[2];
+                }
+                fullcommand = listcommand + " " + pathtolist;
+                system(fullcommand.c_str());
+            } else if (strcmp(argv[1], "-a") == 0){
+                listcommand = "dir /a /b";
+                if (argc < 3){
+                    pathtolist = ".";
+                } else {
+                    pathtolist = argv[2];
+                }
+                fullcommand = listcommand + " " + pathtolist;
+                system(fullcommand.c_str());
+            } else if (strcmp(argv[1], "-h") == 0){
+                helpdialog();
+            } else if (strcmp(argv[1], "--help") == 0){
+                helpdialog();
+            } else {
+                listcommand = "dir /b ";
+                pathtolist = argv[1];
+                fullcommand = listcommand + " " + pathtolist;
+                system(fullcommand.c_str());
+            }
         }
+
     
 }
