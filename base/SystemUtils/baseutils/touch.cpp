9849dca32bd65909c92aff27b342808ee61dd0f1
Hawk2811 <2811.pedro@gmail.com>
2026-02-10 04:35:25 -0300

    Add Help Dialog to touch command

--- base/SystemUtils/baseutils/touch.cpp
+++ base/SystemUtils/baseutils/touch.cpp
@@ -12,8 +12,23 @@ using namespace std;
 namespace fs = std::filesystem;
 
 
+void helpdialog(){
+    cout << "Lukindu Baseutils - touch command" << endl << endl;
+    cout << "touch [filename] - create a new empty file";
+}
+
 
 int main(int argc, char const *argv[]) {
+    if (argc < 2){
+        cout << "Error: No input file specified. try \"-h\" to help";
+    } else {
+        if (strcmp(argv[1], "-h")){
+            helpdialog();
+        } else if (strcmp(argv[1], "--help")){
+            helpdialog();
+        }
+        
+    }
     
     return 0;
 }
