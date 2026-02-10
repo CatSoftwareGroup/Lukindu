ab0eef94b6572ef43d9020a1a6a8a738d6f15698
Hawk2811 <2811.pedro@gmail.com>
2026-02-10 12:00:36 -0300

    bug fix in cat.cpp

--- base/SystemUtils/baseutils/cat.cpp
+++ base/SystemUtils/baseutils/cat.cpp
@@ -19,7 +19,7 @@ int main(int argc, char const *argv[]){
     if (argc < 2) {
         cout << argv[0] << ": Usage '" << argv[0] << " filename'";
     } else {
-        ifstream input({argv[1]});
+        ifstream input(argv[1]);
 
         while (getline (input, text)) {
             cout << text << endl;
