c216a6ea4ddc874ee62198e829c161e1f3d1a13e
Hawk2811 <2811.pedro@gmail.com>
2026-02-10 05:20:29 -0300

    Add helpdialog to rm.cpp

--- base/SystemUtils/baseutils/rm.cpp
+++ base/SystemUtils/baseutils/rm.cpp
@@ -5,10 +5,33 @@
 *
 */
 
+#include <iostream>
+#include <filesystem>
+#include <fstream>
+
+
+using namespace std;
+namespace fs = std::filesystem;
+
+
+void helpdialog(){
+
+}
 
 
 int main(int argc, char const *argv[])
 {
+    if (argc < 2){
+        cout << "Error: No file specified \"rm -h\" to help";
+    } else {
+        if (strcmp(argv[1], "--help") == 0){
+            helpdialog();
+        } else if (strcmp(argv[1], "-h") == 0){
+            helpdialog();
+        }
+        
+        
+    }
     
     return 0;
 }
