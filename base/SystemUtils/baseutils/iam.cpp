9a2947868a9eee0fc46254f1a3cf1a7d60915961
Hawk2811 <2811.pedro@gmail.com>
2026-02-10 03:57:22 -0300

    Created iam command

--- base/SystemUtils/baseutils/iam.cpp
+++ base/SystemUtils/baseutils/iam.cpp
@@ -7,11 +7,15 @@
 
 
 #include <iostream>
+#include <string>
 
+
+using namespace std;
 
 
 int main(int argc, char const *argv[])
 {
-    
+    string LUKINDU_LOGGED_USER = getenv("usrinput");
+    cout << LUKINDU_LOGGED_USER;    
     return 0;
 }
