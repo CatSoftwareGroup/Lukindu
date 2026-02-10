dee68981e441551e28986884aee73088c64b4ecc
Hawk2811 <2811.pedro@gmail.com>
2026-02-10 04:25:06 -0300

    poweroff command Done!

--- base/SystemUtils/baseutils/poweroff.cpp
+++ base/SystemUtils/baseutils/poweroff.cpp
@@ -6,11 +6,22 @@
 */
 
 #include <iostream>
+#include <chrono>
+#include <thread>
+#include <io.h>
 
+using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    
+    cout << "SYSTEM SHUTDOWN NOW" << endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(3000));//Sleep 3 seconds
+    if(_access("\\rootfs\\System\\Library\\shutdownScript.cmd", 0) == 0){//Check if file exists
+        system("\\rootfs\\System\\Library\\shutdownScript.cmd");
+    } else {
+        system("TASKKILL /im cmd.exe");
+        system("taskkill /im mach_kernel.exe");
+    }
     return 0;
 }
 
