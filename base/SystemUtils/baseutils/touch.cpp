fc08f0f969d36bd09f93e0e5fa3131c608420b60
Hawk2811 <2811.pedro@gmail.com>
2026-02-10 04:30:21 -0300

    Start rewritten touch command

---
+++ base/SystemUtils/baseutils/touch.cpp
@@ -0,0 +1,19 @@
+/*
+*
+*    Lukindu - Unix Simulator
+*    touch.cpp
+*
+*/
+#include <iostream>
+#include <filesystem>
+
+
+using namespace std;
+namespace fs = std::filesystem;
+
+
+
+int main(int argc, char const *argv[]) {
+    
+    return 0;
+}
