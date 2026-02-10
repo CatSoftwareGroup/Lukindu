21a43b02d8ac41a7e73a746115af09192445a5ed
Hawk2811 <2811.pedro@gmail.com>
2026-02-10 02:52:24 -0300

    ls.cpp Structure complete

--- base/SystemUtils/baseutils/ls.cpp
+++ base/SystemUtils/baseutils/ls.cpp
@@ -8,3 +8,42 @@
 #include <iostream>
 #include <string>
 #include <filesystem>
+
+
+namespace fs = std::filesystem;
+using namespace std;
+
+
+
+void helpdialog(){
+
+}
+
+int main(int argc, char const *argv[]){
+    string pathToList;
+    if (argc < 2){
+        pathToList = ".";
+    } else {
+        if (argv[1] == "-l"){
+            if (argc < 3){
+
+            }
+        } else if (argv[1] == "-la"){
+            if (argc < 3){
+
+            }
+        } else if (argv[1] == "-a"){
+            if (argc < 3){
+
+            }
+        } else if (argv[1] == "-h"){
+            helpdialog();
+        } else if (argv[1] == "--help"){
+            helpdialog();
+        } else {
+            
+        }
+        
+    }
+    
+}
