541f211991033c345455fbfd4d9decf653851598
Hawk2811 <2811.pedro@gmail.com>
2026-02-10 06:50:03 -0300

    Rewrite of rm command Done!

--- base/SystemUtils/baseutils/rm.cpp
+++ base/SystemUtils/baseutils/rm.cpp
@@ -24,8 +24,34 @@ void helpdialog(){
 }
 
 
-int main(int argc, char const *argv[])
-{
+void deleteFile(string file){
+    try {
+        if (filesystem::is_directory(file)){
+            cerr << "Error on deleting file: Is Directory";
+        } else {
+            fs::remove(file);
+        }
+        
+
+    }
+    catch(const fs::filesystem_error& err) {
+        cerr << "Error on deleting file: " << err.what();
+    }
+    
+}
+
+void deleteAllFiles(string file){
+    try {
+        fs::remove_all(file);   
+    }
+    catch(const fs::filesystem_error& err){
+        cerr << "Error on deleting file: " << err.what();
+    }
+    
+}
+
+
+int main(int argc, char const *argv[]) {
     if (argc < 2){
         cout << "Error: No file specified \"rm -h\" to help";
     } else {
@@ -33,12 +59,19 @@ int main(int argc, char const *argv[])
             helpdialog();
         } else if (strcmp(argv[1], "-h") == 0){
             helpdialog();
+        } else if (strcmp(argv[1], "-r") == 0){
+            deleteAllFiles(argv[2]);
+        } else if (strcmp(argv[1], "-f") == 0){
+            deleteFile(argv[2]);
+        } else if (strcmp(argv[1], "-rf") == 0){
+            deleteAllFiles(argv[2]);
         } else {
-
+            deleteFile(argv[1]);
         }
-        
-        
+           
     }
     
-    return 0;
+    return 0; 
 }
+    
+    
